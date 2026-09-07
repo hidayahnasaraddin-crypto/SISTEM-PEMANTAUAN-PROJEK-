e79ca90d55a71722af5317201ab4578eaf6431ea
hidayahnasaraddin-crypto <hidayah.nasaraddin@moh.gov.my>
2026-09-07 13:49:54 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-iframe-cache-v2';
+const CACHE_NAME = 'pwa-iframe-cache-v3';
 
 // Senarai fail yang perlu disimpan secara luar talian (offline)
 // Nota: Kita tidak boleh cache kandungan dalam iframe secara langsung,
